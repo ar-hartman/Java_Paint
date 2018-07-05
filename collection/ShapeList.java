06713f6c66235b4c4b76765ebe3f1d387c9b0228
Addison Hartman <ar.hartman@hotmail.com>
2018-07-05 12:44:50 -0700

    saving all changes

---
+++ collection/ShapeList.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.ArrayList;
+
+import model.Shape;
+
+public class ShapeList {
+	ArrayList<Shape> shapes;
+	public ShapeList() {
+		shapes = new ArrayList<Shape>();
+	}
+	
+	public void addShape(Shape shape) {
+		this.addShape(shape);
+	}
+}
