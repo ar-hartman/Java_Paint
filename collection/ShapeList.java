ac7ad015179851dc9320f4a4ca6ea5593ac12456
Addison Hartman <ar.hartman@hotmail.com>
2018-07-22 14:18:36 -0700

    Create ShapeList.java

---
+++ collection/ShapeList.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.ArrayList;
+
+import command.Command;
+import model.Shape;
+
+public class ShapeList {
+	ArrayList<Shape> shapeList = new ArrayList<Shape>();
+
+	public void addShape(Shape shape) {
+		shapeList.add(shape);
+		System.out.println("something added - triggered by addShape methof from ShapeList class");
+	}
+	/*
+	public void drawShapes() {
+		for (Shape shape : shapeList) {
+			command.execute();
+		}
+	}*/
+	public ArrayList<Shape> getShapeList(){
+		return shapeList;
+	}
+}
