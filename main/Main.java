7d391840d24cd4b5cd207e523b83e3410f2942a0
Addison Hartman <ar.hartman@hotmail.com>
2018-07-22 14:18:08 -0700

    Add files via upload

--- main/Main.java
+++ main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
+import collection.ShapeList;
+import command.CommandList;
 import controller.IJPaintController;
 import controller.JPaintController;
+import model.Shape;
+import model.ShapeBuilder;
+import model.ShapeListObserver;
 import model.dialogs.DialogProvider;
 import model.interfaces.IDialogProvider;
 import model.persistence.ApplicationState;
+import mouse.MouseHandler;
 import view.gui.Gui;
 import view.gui.GuiWindow;
 import view.gui.PaintCanvas;
@@ -12,11 +18,36 @@ import view.interfaces.IGuiWindow;
 import view.interfaces.IUiModule;
 
 public class Main {
+	
+
     public static void main(String[] args){
-        IGuiWindow guiWindow = new GuiWindow(new PaintCanvas());
+    	/*
+    	 * adding during test
+    	 */
+        //ShapeBuilder builder = new ShapeBuilder();
+        /*
+         * 
+         */
+    	MouseHandler mouseHandler = new MouseHandler();
+    	IGuiWindow guiWindow = new GuiWindow(new PaintCanvas(), mouseHandler);
         IUiModule uiModule = new Gui(guiWindow);
         ApplicationState appState = new ApplicationState(uiModule);
+        
+        //IDialogProvider dialog = new DialogProvider(appState);
         IJPaintController controller = new JPaintController(uiModule, appState);
         controller.setup();
+
+        /*
+         * 
+         */
+        //guiWindow.setApplicationState(appState);
+        //builder.setShapeType(appState.getActiveShapeType());
+        //System.out.println("Shape Type: " + appState.getActiveShapeType());
+        /*
+         * 
+         */
+        
+        ShapeBuilder builder = new ShapeBuilder(mouseHandler, appState);
+        ShapeListObserver shapeListObserver = new ShapeListObserver(builder);       
     }
 }
