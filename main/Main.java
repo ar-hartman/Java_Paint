9c5592e9f17a815104cec3c9c092a50eb5258b07
Addison Hartman <ar.hartman@hotmail.com>
2018-08-12 14:10:25 -0700

    Finished School Project

--- main/Main.java
+++ main/Main.java
@@ -2,6 +2,11 @@ package main;
 
 import controller.IJPaintController;
 import controller.JPaintController;
+import model.BuilderObserver;
+import model.MoveObserver;
+import model.Mover;
+import model.Selector;
+import model.SelectorObserver;
 import model.ShapeBuilder;
 import model.persistence.ApplicationState;
 import mouse.MouseHandler;
@@ -10,6 +15,23 @@ import view.gui.GuiWindow;
 import view.gui.PaintCanvas;
 import view.interfaces.IGuiWindow;
 import view.interfaces.IUiModule;
+
+/*
+ * Description: 
+ * The Main method was provided by the instructor, but was modified to include functionality
+ * required to complete the assigned project.  
+ * 
+ * This included creation of:
+ * - a PaintCanvas object outside of the GuiWindow object initialization. 
+ * - a MouseHandler object, essential action listening within the GuiWindow 
+ * 		and also for the three observer objects that were created.
+ * - a BuilderObserver object, responsible for being notified of Mouse activity and acquiring
+ * 		application state data.
+ * - a SelectorObserver object, responsible selecting a Shape only when certain mouse actions were
+ * 		triggered along with specific application state criteria.
+ * - a MoveObserver, responsible for moving a selected Shape only when certain mouse action were 
+ * 		triggered along with specific application state criteria.   
+ */
 
 public class Main {
 	
@@ -24,6 +46,8 @@ public class Main {
         ApplicationState appState = new ApplicationState(uiModule);
         IJPaintController controller = new JPaintController(uiModule, appState);
         controller.setup();
-        ShapeBuilder builder = new ShapeBuilder(mouseHandler, appState, canvas);
+        BuilderObserver builder = new ShapeBuilder(mouseHandler, appState, canvas);
+        SelectorObserver selecter = new Selector(mouseHandler, appState, canvas);
+        MoveObserver mover = new Mover(mouseHandler, appState, canvas);
     }
 }
